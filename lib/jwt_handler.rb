8894698877d8c416453f760c4eeb0a0baad28c4d
Bletsk <blackrid2011@gmail.com>
2018-06-01 19:22:27 +0300

    env support

--- lib/jwt_handler.rb
+++ lib/jwt_handler.rb
@@ -11,21 +11,20 @@ module JWTHandler
   included do
     before_action :validate_token
 
-    module ClassMethods
-	    attr_reader :arguable_opts
+ #    module ClassMethods
+	#     attr_reader :arguable_opts
 
-	    private
-	    def jwt_parameters(opts={})
-	    	@arguable_opts = opts
-	    end
-	end
+	#     private
+	#     def jwt_parameters(opts={})
+	#     	@arguable_opts = opts
+	#     end
+	# end
 
     def validate_token
-    	p ENV['program_service_url']
   		return if ['api/v1/auth'].include?(params[:controller])
 
-		jwt_validation_path = get_validation_path_from_opts
-		referer = get_ref_link_from_opts
+		jwt_validation_path = get_auth_service_path + '/api/v1/session/validate'
+		referer = get_ref_link
 
 		headers = { 
 		"Authorization"  => get_jwt()
@@ -49,29 +48,9 @@ module JWTHandler
 			else
 				render json:{redirect_url:redirect_url}, status: 302
 			end
-
-			# respond_to do |format|
-			# 	format.json {
-			# 		render json: {
-			#         	error: "Error: Not authorized \\ Wrong token"
-			# 		}, :status => 401
-			# 	}
-			# 	format.html {
-			# 		redirect_url = parsed_body['sign_in_url']
-			# 		if !referer.to_s.blank?
-			# 			redirect_url += "?redirect_url=#{referer}" #we have to send back redirection url
-			# 		end
-
-			# 		if !request.headers['HTTP_ACCEPT'].include?("application/json") #checkout for ajax requests
-			# 			redirect_to redirect_url
-			# 		else
-			# 			render json:{redirect_url:redirect_url}, status: 302
-			# 		end
-			# 	}
-			# end
 		else
 			if !parsed_body['updated_token'].blank? #if jwt updated
-			    cookies['JWT'] = response.set_cookie "JWT", { :value => parsed_body['updated_token'], :domain => get_domain_name_from_opts}
+			    cookies['JWT'] = response.set_cookie "JWT", { :value => parsed_body['updated_token'], :domain => get_domain_name }
 		    end
 		end
 	end
@@ -91,57 +70,71 @@ module JWTHandler
 		end
 
 	#Получаем данные из переданных аргументов
-	private
-	def get_arguable_opts
-	    if self.class.arguable_opts.blank? && self.class.superclass.arguable_opts.present?
-	    	opts = self.class.superclass.arguable_opts
-	    elsif self.class.arguable_opts.present? && self.class.superclass.arguable_opts.present?
-	    	opts = self.class.superclass.arguable_opts.merge(self.class.arguable_opts)
-	    else
-	    	opts = self.class.arguable_opts
-	    end
-	    opts || {}
-  	end
+	# private
+	# def get_arguable_opts
+	#     if self.class.arguable_opts.blank? && self.class.superclass.arguable_opts.present?
+	#     	opts = self.class.superclass.arguable_opts
+	#     elsif self.class.arguable_opts.present? && self.class.superclass.arguable_opts.present?
+	#     	opts = self.class.superclass.arguable_opts.merge(self.class.arguable_opts)
+	#     else
+	#     	opts = self.class.arguable_opts
+	#     end
+	#     opts || {}
+ #  	end
 
   	#Проверяем в параметрах наличие реферальной ссылки
+  	# private
+  	# def get_ref_link_from_opts
+  	# 	opts = get_arguable_opts
+  	# 	unless opts[:ref_link].to_s.blank?
+  	# 		return opts[:ref_link]
+  	# 	end
+
+  	# 	if !opts[:controller].to_s.blank? && !opts[:action].to_s.blank?
+  	# 		if !opts[:id].to_s.blank?
+  	# 			return url_for(controller: opts[:controller], action: opts[:action], id: opts[:id])
+  	# 		end
+
+  	# 		return url_for(controller: opts[:controller], action: opts[:action])
+  	# 	end
+
+  	# 	return 'http://localhost:3000/organization'
+  	# end
+
+  	# #Проверяем в параметрах наличие ссылки на сервер валидации
+  	# def get_validation_path_from_opts
+  	# 	opts = get_arguable_opts
+
+  	# 	unless opts[:validation_path].to_s.blank?
+  	# 		return opts[:validation_path]
+  	# 	end
+
+  	# 	return 'http://localhost:3001/api/v1/session/validate'
+  	# end
+
+  	# # Проверяем в параметрах наличие имени домена
+  	# def get_domain_name_from_opts
+  	# 	opts = get_arguable_opts
+
+  	# 	unless opts[:domain_name].to_s.blank?
+  	# 		return opts[:domain_name]
+  	# 	end
+
+  	# 	return 'localhost'
+  	# end
+
   	private
-  	def get_ref_link_from_opts
-  		opts = get_arguable_opts
-  		unless opts[:ref_link].to_s.blank?
-  			return opts[:ref_link]
-  		end
-
-  		if !opts[:controller].to_s.blank? && !opts[:action].to_s.blank?
-  			if !opts[:id].to_s.blank?
-  				return url_for(controller: opts[:controller], action: opts[:action], id: opts[:id])
-  			end
-
-  			return url_for(controller: opts[:controller], action: opts[:action])
-  		end
-
-  		return 'http://localhost:3000/organization'
+  	def get_ref_link
+  		ENV['jwt_referer_link'] || Rails.root
   	end
 
-  	#Проверяем в параметрах наличие ссылки на сервер валидации
-  	def get_validation_path_from_opts
-  		opts = get_arguable_opts
-
-  		unless opts[:validation_path].to_s.blank?
-  			return opts[:validation_path]
-  		end
-
-  		return 'http://localhost:3001/api/v1/session/validate'
+  	def get_auth_service_path
+  		ENV['jwt_auth_service_path'] || 'http://localhost:3001'
   	end
 
-  	# Проверяем в параметрах наличие имени домена
-  	def get_domain_name_from_opts
-  		opts = get_arguable_opts
-
-  		unless opts[:domain_name].to_s.blank?
-  			return opts[:domain_name]
-  		end
-
-  		return 'localhost'
+  	def get_domain_name
+  		ENV['jwt_domain_name'] || 'localhost'
   	end
+
   end
 end
