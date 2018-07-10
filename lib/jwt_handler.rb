765abe4bb7001a299c1680f235e9ab00a7360813
Bletsk <blackrid2011@gmail.com>
2018-07-10 19:11:49 +0300

    Реструктуризация гема

--- lib/jwt_handler.rb
+++ lib/jwt_handler.rb
@@ -9,67 +9,12 @@ include ActionController::Cookies
 module JWTHandler
   extend ActiveSupport::Concern
   included do
-    before_action :validate_token
+    before_action :init
 
-  def validate_token
-    
-    # Скипаем валидацию, если в контроллере аутентификации
-    return if ['api/v1/auth'].include?(params[:controller]) || ['mail_auth'].include?(params[:controller])
+  def init
+    return if check_for_excluded_controllers || check_for_debug || check_for_x_authorization || check_for_testing_environments
 
-    # Скипаем валидацию, если запрос в режиме дебага
-    return p "JWT: Page rendered in debug-mode" if check_for_debug
-
-    # @t = Thread.new do
-    # Скипаем валидацию, если предоставлен секрет
-    if request.headers['X-Authorization']
-      # logger.info "Обнаружен секрет. Проверяю..."
-      
-        # begin
-      headers = {
-        "X-Authorization" => get_secret()
-      }
-
-      path = get_user_management_path + '/api/v1/auth/get_user_data_by_secret'
-      response = HTTParty.get(path, :headers => headers, :timeout => 20)
-      logger.info headers
-      logger.info response
-      if response.code.to_s.include?("20")
-          return @user = JSON.parse(response.body)
-      else
-          # p "jwt: Секрет неверен"
-          validate_jwt
-      end
-    else
-      # logger.info "jwt: Секрет не задан"
-
-      uri = URI.parse(request.original_url)
-      token = CGI.parse(uri.query)['token'][0] if uri.query
-      if token
-        redirect_url = get_user_management_path + '/api/v1/auth/token/' + token + '?redirect_url=' + request.url.split('?').first
-
-        return redirect_to redirect_url unless request.headers['HTTP_ACCEPT'].include?("application/json") 
-        
-        return render json:{redirect_url:redirect_url}, status: 302
-      else
-        validate_jwt
-      end
-    end
-        # rescue HTTParty::Error => e
-        #   p "JWT-handler: HTTParty error"
-        #   p e.inspect
-        # rescue StandardError => e
-        #   "JWT-handler: StandardError"
-        #   p e.inspect
-        # end
-
-        # return render json: {
-        #   error: "X-Authorization error"
-        # }, :status => 401
-      # end
-        
-      # Скипаем валидацию в development-окружении
-
-    # end
+    check_for_auth_token
   end
 
   def validate_jwt
@@ -102,7 +47,6 @@ module JWTHandler
       render json:{redirect_url:redirect_url}, status: 302
     else
       #if jwt updated
-      # logger.info "Валидация успешна"
       unless parsed_body['updated_token'].blank?
         cookies['JWT'] = { :value => parsed_body['updated_token'], :domain => get_domain_name, :path => '/' }
       end
@@ -130,7 +74,7 @@ module JWTHandler
     return JWT.decode(token, nil, false)[0]
   end
 
-  # Запрашиваем данные текущего пользователя
+  # Возвращаем данные текущего пользователя
   def current_user
     if request.headers['X-Authorization']
       # @t.join
@@ -155,26 +99,72 @@ module JWTHandler
     return {}
   end
 
-    private
-    def get_ref_link
-      ENV['jwt_referer_link'] || Rails.root
-    end
+  private
+  def get_ref_link
+    ENV['jwt_referer_link'] || Rails.root
+  end
 
-    def get_auth_service_path
-      ENV['jwt_auth_service_path'] || 'http://localhost:3001'
-    end
+  def get_auth_service_path
+    ENV['jwt_auth_service_path'] || 'http://localhost:3001'
+  end
 
-    def get_user_management_path
-      ENV['jwt_user_management_path'] || ENV['user_management_url'] || 'http://localhost:3023'
-    end
+  def get_user_management_path
+    ENV['jwt_user_management_path'] || ENV['user_management_url'] || 'http://localhost:3023'
+  end
 
-    def get_domain_name
-      ENV['jwt_domain_name'] || 'localhost'
-    end
+  def get_domain_name
+    ENV['jwt_domain_name'] || 'localhost'
+  end
 
-    def check_for_debug
-      uri = URI.parse(request.original_url)
-      return uri.query && CGI.parse(uri.query)['jwt-debug'][0] == 'true'
+  # Проверяем, не входит ли текущий контроллер в список исключенных из проверки
+  def check_for_excluded_controllers
+    return ['api/v1/auth'].include?(params[:controller]) || ['mail_auth'].include?(params[:controller])
+  end
+
+  # Проверяем, не открыта ли текущая страница в режиме дебага гема
+  def check_for_debug
+    uri = URI.parse(request.original_url)
+    return uri.query && CGI.parse(uri.query)['jwt-debug'][0] == 'true'
+  end
+
+  # Проверяем, находимся ли мы в тестовом окружении
+  def check_for_testing_environments
+    return (Rails.env.development? || Rails.env.test?) && !(ENV['jwt_validate_on_test'] == "true")
+  end
+
+  # Проверяем на наличие и валидность секрета в параметрах
+  def check_for_x_authorization
+    if request.headers['X-Authorization']
+      headers = {
+        "X-Authorization" => get_secret()
+      }
+
+      path = get_user_management_path + '/api/v1/auth/get_user_data_by_secret'
+      response = HTTParty.get(path, :headers => headers, :timeout => 20)
+      # logger.info headers
+      # logger.info response
+      if response.code.to_s.include?("20")
+          @user = JSON.parse(response.body)
+          return true
+      else
+          return false
+      end
     end
   end
+
+  # Проверяем на наличие и валидность токена авторизации в параметрах
+  def check_for_auth_token
+    uri = URI.parse(request.original_url)
+    token = CGI.parse(uri.query)['token'][0] if uri.query
+    if token
+      redirect_url = get_user_management_path + '/api/v1/auth/token/' + token + '?redirect_url=' + request.url.split('?').first
+
+      return redirect_to redirect_url unless request.headers['HTTP_ACCEPT'].include?("application/json") 
+      
+      return render json:{redirect_url:redirect_url}, status: 302
+    else
+      validate_jwt
+    end
+  end
+
 end
